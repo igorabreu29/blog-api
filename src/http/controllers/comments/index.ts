81f419f0787e24491764987275d65dc349d5573e
igorabreu29 <abreusense@gmail.com>
2025-01-14 19:47:19 -0300

    feat: add delete comment to register

--- src/http/controllers/comments/index.ts
+++ src/http/controllers/comments/index.ts
@@ -1,8 +1,10 @@
 import type { FastifyTypedInstance } from '@/types.ts'
 import { addCommentToPost } from './add-comment-to-post.ts'
 import { editComment } from './edit-comment.ts'
+import { deleteComment } from './delete-comment.ts'
 
 export async function commentRoutes(app: FastifyTypedInstance) {
 	app.register(addCommentToPost)
 	app.register(editComment)
+	app.register(deleteComment)
 }
