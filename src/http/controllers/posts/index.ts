f45f9377f14449fdd05e9cf0db62f4f2406c7d37
igorabreu29 <abreusense@gmail.com>
2025-01-14 19:47:58 -0300

    feat: add get posts controller to register

--- src/http/controllers/posts/index.ts
+++ src/http/controllers/posts/index.ts
@@ -2,8 +2,10 @@ import type { FastifyTypedInstance } from '@/types.ts'
 import { createPost } from './create-post.ts'
 import { editPost } from './edit-post.ts'
 import { deletePost } from './delete-post.ts'
+import { getPosts } from './get-posts.ts'
 
 export async function postRoutes(app: FastifyTypedInstance) {
+	app.register(getPosts)
 	app.register(createPost)
 	app.register(editPost)
 	app.register(deletePost)
