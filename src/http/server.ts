ba3ddc1a1a530eea0ba03d77cb03c23619e3f056
igorabreu29 <abreusense@gmail.com>
2025-01-21 15:40:07 -0300

    refactor:  update console

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,5 +1,5 @@
-import { app } from "@/app.ts";
-import { env } from "@/env/index.ts";
+import { app } from '@/app.ts'
+import { env } from '@/env/index.ts'
 
 const url = await app.listen({ port: env.PORT })
-console.log(`Server running on url ${url}`)
+console.log(`Server running on url ${url} (/docs)`)
