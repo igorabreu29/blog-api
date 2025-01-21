14d78b0a58912537a780def6c65732354c673849
igorabreu29 <abreusense@gmail.com>
2025-01-21 15:42:07 -0300

    feat: add option to change user inform before auth

--- test/factories/make-auth.ts
+++ test/factories/make-auth.ts
@@ -1,9 +1,10 @@
 import request from 'supertest'
 import { makeUser } from './make-user.ts'
 import { app } from '@/app.ts'
+import type { User } from '@/http/controllers/users/types/user.ts'
 
-export async function makeAuth() {
-	const user = await makeUser({ name: 'John Doe' })
+export async function makeAuth(override: Partial<User> = {}) {
+	const user = await makeUser({ name: 'John Doe', ...override })
 	const response = await request(app.server).get(
 		`/authenticate?email=${user.email}`
 	)
